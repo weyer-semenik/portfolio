190457c8062229ee3ded6cefa8360fbb20af38f5
weyer-semenik <weyer.rm@gmail.com>
2017-09-06 23:26:26 +0200

    portfolio

--- js/script.js
+++ js/script.js
@@ -1,11 +1,9 @@
 const aboutOverlay = document.getElementById('js-aboutOverlay'),
     projectOverlay = document.getElementById('js-projectOverlay'),
-    cvOverlay = document.getElementById('js-cvOverlay'),
     socialOverlay = document.getElementById('js-socialOverlay'),
     overlayHome = document.getElementsByClassName('overlay-home'),
     overlayAbout = document.getElementsByClassName('overlay-about'),
     overlayProject = document.getElementsByClassName('overlay-project'),
-    overlayCV = document.getElementsByClassName('overlay-cv'),
     overlaySocial = document.getElementsByClassName('overlay-social'),
     overlayHomeHeader = document.getElementById('js-overlayHomeHeader'),
     overlayHomeList = document.getElementById('js-overlayHomeList');
@@ -18,7 +16,6 @@ function menuStateChoose(e) {
 
 aboutOverlay.addEventListener('click', function () { menuStateChoose('about'); });
 projectOverlay.addEventListener('click', function () { menuStateChoose('project'); });
-cvOverlay.addEventListener('click', function () { menuStateChoose('cv'); });
 socialOverlay.addEventListener('click', function () { menuStateChoose('social'); });
 
 var menuState = 'home';
@@ -29,28 +26,18 @@ function setMenuState() {
             overlayHomeHeader.style.display = 'none';
             overlayAbout[0].style.display = 'block';
             overlayProject[0].style.display = 'none';
-            overlayCV[0].style.display = 'none';
             overlaySocial[0].style.display = 'none';
             break;
         case 'project':
             overlayHomeHeader.style.display = 'none';
             overlayAbout[0].style.display = 'none';
             overlayProject[0].style.display = 'flex';
-            overlayCV[0].style.display = 'none';
-            overlaySocial[0].style.display = 'none';
-            break;
-        case 'cv':
-            overlayHomeHeader.style.display = 'none';
-            overlayAbout[0].style.display = 'none';
-            overlayProject[0].style.display = 'none';
-            overlayCV[0].style.display = 'block';
             overlaySocial[0].style.display = 'none';
             break;
         case 'social':
             overlayHomeHeader.style.display = 'none';
             overlayAbout[0].style.display = 'none';
             overlayProject[0].style.display = 'none';
-            overlayCV[0].style.display = 'none';
             overlaySocial[0].style.display = 'block';
             break;
         case 'home':
@@ -58,7 +45,6 @@ function setMenuState() {
             overlayHomeHeader.style.display = 'block';
             overlayAbout[0].style.display = 'none';
             overlayProject[0].style.display = 'none';
-            overlayCV[0].style.display = 'none';
             overlaySocial[0].style.display = 'none';
     }
 }
